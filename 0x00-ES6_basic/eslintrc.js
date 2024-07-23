0b1d783204d18930a79259e865dad41a60c6449f
sanotogii <nasaereh@gmail.com>
2024-07-23 16:04:03 +0200

    update

---
+++ 0x00-ES6_basic/eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+        browser: false,
+        es6: true,
+        jest: true,
+    }, extends: [
+        'airbnb-base',
+        'plugin:jest/recommended',
+    ], globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    }, parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+    }, plugins: ['jest'],
+    rules: {
+        'no-console': 'off',
+        'no-shadow': 'off',
+        'no-restricted-syntax': [
+            'error',
+            'LabeledStatement',
+            'WithStatement',
+        ],
+    },
+    overrides:[
+        {
+            files: ['*.js'],
+            excludedFiles: 'babel.config.js',
+        }
+    ]
+};
