4a24c577e27f96d1d7b02619149ec907b0410470
sanotogii <nasaereh@gmail.com>
2024-09-18 17:10:41 +0200

    Refactor stdin.js: remove unnecessary code and add end event listener

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,13 +3,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
-
-  if (process.stdin.isTTY) {
-    process.stdin.resume();
-  } else {
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
-  }
 });
-process.stdin.resume();
-
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
